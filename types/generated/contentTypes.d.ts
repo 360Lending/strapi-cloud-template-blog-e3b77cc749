dbd16291d35933ac9d9e0c62e445e65d569f1db6
Jacky Tchen <jacky@finnovate.io>
2024-04-09 08:58:05 -0600

    Change homepage collection field names

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -933,12 +933,12 @@ export interface ApiHomePageHomePage extends Schema.CollectionType {
   };
   attributes: {
     title_small: Attribute.String;
-    carousel_large_line1: Attribute.String;
-    carousel_small: Attribute.String;
+    carousel_header_line1: Attribute.String;
+    carousel_subheader: Attribute.String;
     get_started_button: Attribute.String;
     learn_more_button: Attribute.String;
     slug: Attribute.UID;
-    carousel_large_line2: Attribute.String;
+    carousel_header_line2: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
