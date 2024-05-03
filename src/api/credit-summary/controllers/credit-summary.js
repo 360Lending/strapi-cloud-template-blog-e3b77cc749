ba2ff22330a29fdb30ae450bbb262ed1ae9e61c7
Jacky Tchen <jacky@finnovate.io>
2024-05-03 12:01:59 -0600

    Add credit summary collection

---
+++ src/api/credit-summary/controllers/credit-summary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * credit-summary controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::credit-summary.credit-summary');
---
+++ src/api/credit-summary/routes/credit-summary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * credit-summary router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::credit-summary.credit-summary');
