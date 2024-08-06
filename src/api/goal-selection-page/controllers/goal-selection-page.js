6c0aa09b647663be23adab8c7128d0477c074609
Kevin Baik <kevinbaik@Kevins-MacBook-Pro.local>
2024-08-06 11:09:26 -0400

    add goal fields

---
+++ src/api/goal-selection-page/controllers/goal-selection-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * goal-selection-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::goal-selection-page.goal-selection-page');
